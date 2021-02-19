dbac819e1d99ddcf444343222ad46c8119351f43
Preeti Sharma <52451741+PreetiSharma2815@users.noreply.github.com>
2021-02-19 12:00:57 +0530

    Update Compounds.js

--- Compounds.js
+++ Compounds.js
@@ -71,17 +71,11 @@ AFRAME.registerComponent("molecules", {
 
 
             electron.setAttribute("material", "color", "yellow")
-            //if (n==0)
+           
             electron.setAttribute("position", { x: orbitRadius, y: 1, z: orbitRadius })
-            //if (n==1) electron.setAttribute("position", { x:  orbitRadius-0.01 , y: 1, z: orbitRadius })
-            //electron.setAttribute("position", { x:  orbitRadius*Math.cos((2*Math.PI)/numOfElectron)*n , y: 1, z: orbitRadius*Math.sin((2*Math.PI)/numOfElectron)*n })
-            //electron.setAttribute({x:orbitRadius * Math.cos(n * 6.283/8),y: 1, z:orbitRadius * Math.sin(n * 6.283/8)});
-            //electron.setAttribute("animation", { property: "rotation", to: " 0 360 0" })
-            //console.log(electron)
+            
             electronGroup.appendChild(electron);
-        }
-
-        //console.log(artoolkit.getMarker())
+        }        
 
         electronGroup.setAttribute("animation", {
             property: "rotation",
@@ -90,61 +84,7 @@ AFRAME.registerComponent("molecules", {
             dur: 2000,
         });
 
-
-        // marker.addEventListener('getMarker', function (ev) {
-        //     console.log('found marker?', ev);
-        // });
-
-        //console.log(THREEx.ArMarkerControls)
-        return marker;
+        //return marker;
     },
-    tick: function(){
-        this.createCompounds()
-    },
-    createCompounds: function () {
-
-        var marker1Pos, marker2Pos
-        
-        var marker1 = document.querySelector("#barcodeMarker3")
-        var marker2 = document.querySelector("#barcodeMarker4")
-
-        
-        marker1Pos = new THREE.Vector3();
-        marker1.object3D.getWorldPosition(marker1Pos);
-        
-
-        marker2Pos = new THREE.Vector3();
-        marker2.object3D.getWorldPosition(marker2Pos);
-        
-        //distance
-        this.d = marker1Pos.distanceTo(marker2Pos);
-        console.log("distance"+this.d)
-
-        if(Math.round(this.d)<2){
-            // this.createMolecules(4, "NaCl", "#BDE1F3", "0.08", 0)
-            var c = marker2.children[0];
-            c.setAttribute("material", {
-                src: "./images/card-NaCl.png",
-            })
-        }
-        else{
-            var c = marker2.children[0];
-            c.setAttribute("material", {
-                src: "./images/card-Cl.png",
-            })
-        }       
-
-    },
-    updateMarker: function(){
-        //if d<1
-        //1--> remove electron, position nucleus
-        //2--> remove e, card
-        //else
-        //original
-
-        //if d<1
-        //create the bond marker??
-
-    }
 
 });
